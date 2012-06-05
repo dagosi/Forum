72dfb48eb8a013d40c13e3464b4c77902ec0ecfd
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-06-05 17:44:39 -0400

    Fixed minor bug.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'byd9AA73ghWfsgvguMs9hg', 'dHqCsHZKevtKHjTjLlqb3s1YFiOA90LURralBREei4'
-  provider :facebook, '341013839283786', '87a87e17c3774242410343d1f55fa646', { :scope => 'email, offline_access, user_photos, publish_stream', :client_options => { :ssl => { :ca_path => "/etc/ssl/certs" } } }
+  provider :facebook, '341013839283786', '87a87e17c3774242410343d1f55fa646', { :scope => 'email, offline_access', :Display => 'popup', :client_options => { :ssl => { :ca_path => "/etc/ssl/certs" } } }
   provider :linkedin, 'dmj5hvgs987m', 'tUxpmTkdnmlcsTTH'
 end
